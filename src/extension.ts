053763d86c1663e14538e7255c17e1934b17895c
Ramnivas Laddad <ramnivas@exograph.dev>
2025-03-13 12:25:45 -0700

    Enable LSP to use exo-lsp
    Goes with https://github.com/exograph/exograph/pull/1537

---
+++ src/extension.ts
@@ -0,0 +1,83 @@
+import { workspace, ExtensionContext } from 'vscode';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient | undefined;
+
+const EXO_LSP_ENABLED_KEY = 'exograph.lsp.enabled';
+
+export function activate(context: ExtensionContext) {
+  // Initial activation based on setting
+  if (getExoLspEnabled()) {
+    startLspClient(context);
+  }
+
+  // Listen for configuration changes
+  context.subscriptions.push(
+    workspace.onDidChangeConfiguration(async (event) => {
+      if (event.affectsConfiguration(EXO_LSP_ENABLED_KEY)) {
+        if (getExoLspEnabled()) {
+          startLspClient(context);
+        } else {
+          await deactivate();
+        }
+      }
+    })
+  );
+}
+
+export async function deactivate(): Promise<void> {
+  if (client) {
+    const result = await client.stop();
+    client = undefined;
+    return result;
+  }
+}
+
+function startLspClient(context: ExtensionContext) {
+  if (client) {
+    return;
+  }
+
+  const command = process.env.EXO_LSP_SERVER_PATH || "exo-lsp";
+  const logLevel = process.env.EXO_LSP_LOG || "warn";
+
+  const serverOptions: ServerOptions = {
+    command,
+    options: {
+      env: {
+        "EXO_LSP_LOG": logLevel,
+        "EXO_LSP_LOG_DIR": context.logUri
+      }
+    },
+    transport: TransportKind.stdio
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'exograph' }],
+    synchronize: {
+      fileEvents: workspace.createFileSystemWatcher('**/*.exo')
+    }
+  };
+
+  client = new LanguageClient(
+    'exographLanguageServer',
+    'Exograph Language Server',
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
+}
+
+
+
+
+function getExoLspEnabled() {
+  return workspace.getConfiguration().get(EXO_LSP_ENABLED_KEY, false);
+}
+
